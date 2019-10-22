46d2a435c1a1208ff1bbafd20c70d2c2f06a0178
A Thomas Bianchi <atbianchi@gmail.com>
2019-10-22 19:24:31 -0400

    initial commit

---
+++ src/bid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Bid extends React.Component {
+    render() {
+        return (
+            <div>
+                <input ></input>
+                <button>Submit Bid</button>
+            </div>
+        );
+    }
+}
