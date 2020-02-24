e21399b54d6190639267b07262b67e3cc342519f
A Thomas Bianchi <atbianchi@gmail.com>
2020-02-23 21:23:48 -0500

    atb

--- src/bid.js
+++ src/bid.js
@@ -1,39 +1,14 @@
 import React from 'react';
 
 export default class Bid extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            bid: '',
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleDecline = this.handleDecline.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({ bid: event.target.value })
-    }
-
-    handleSubmit(event) {
-        alert('you submitted a bid of ' + this.state.bid);
-        event.preventDefault();
-    }
-
-    handleDecline(event) {
-        alert('You declined to bid');
-        event.preventDefault();
-    }
-
-
     render() {
         return (
             <form>
-                <div>Your Bid {this.state.bid}</div>
+                <div>Your Bid {this.props.bid}</div>
                 <label htmlFor="bid">Bid</label>
-                <input type="number" min="1" max="100" step="any" value={this.state.bid} onChange={this.handleChange} name="bid"></input>
-                <button onClick={this.handleSubmit}>Submit Bid</button>
-                <button onClick={this.handleDecline}>Decline Bid</button>
+                <input type="number" min="1" max="100" step="any" value={this.props.bid} onChange={this.props.handleChange} name="bid"></input>
+                <button onClick={this.props.handleSubmit}>Submit Bid</button>
+                <button onClick={this.props.handleDecline}>Decline Bid</button>
             </form>
         );
     }
--- src/index.js
+++ src/index.js
@@ -10,18 +10,49 @@ class App extends React.Component {
         this.state = {
             player: {
                 name: 'Mike Trout',
-                team: 'Orlando Renegade'
+                team: 'Orlando Renegades',
+                arbYear: 1
             },
-            biddingTeam: 'Young Bucks'
-        }
+            biddingTeam: 'Young Bucks',
+            bid: '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleDecline = this.handleDecline.bind(this);
     }
+
+    handleChange(event) {
+        this.setState({ bid: event.target.value })
+    }
+
+    handleSubmit(event) {
+        alert('you submitted a bid of ' + this.state.bid);
+        event.preventDefault();
+    }
+
+    handleDecline(event) {
+        alert('You declined to bid');
+        event.preventDefault();
+    }
+
     render() {
+        // const calculated_arb = 
+
         return (
             <div>
                 {/* Timer */}
-                <Player name={this.state.player.name} team={this.state.player.team} />
+                <Player
+                    name={this.state.player.name}
+                    team={this.state.player.team}
+
+                />
                 <Team team={this.state.biddingTeam} />
-                <Bid />
+                <Bid
+                    handleSubmit={this.handleSubmit}
+                    handleDecline={this.handleDecline}
+                    handleChange={this.handleChange}
+                    bid={this.state.bid}
+                />
                 {/* Schedule / Results */}
             </div>
         );
